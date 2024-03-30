fa9feb161561e9e626d587efd626d2278db7564a
brookevillano <104024875+brookevillano@users.noreply.github.com>
2024-03-30 01:49:49 -0700

    Update App.js
    Further setting up the layout of the landing page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import "./assets/fonts/fonts.css"; // Ensure this path is correct
 import "./App.css"; 
+import Filters from "./components/Filters/Filters";
+
 
 function App() {
   return (
@@ -11,6 +13,14 @@ function App() {
           Rick & Morty <span className="text-primary">WiKi</span>
         </h1>
       </header>
+      <div className="container ubuntu">
+        <div className="row">
+          <div className="col-3 body">
+            <Filters /> 
+          </div>
+          <div className="col-8"></div>
+        </div>
+      </div>
     </div>
   );
 }
