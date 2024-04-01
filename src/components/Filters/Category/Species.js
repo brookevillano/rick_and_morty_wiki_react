ebe715c80abf79507faba171a622e7de654fe24e
brookevillano <104024875+brookevillano@users.noreply.github.com>
2024-04-01 16:16:48 -0700

    Create Species.js

---
+++ src/components/Filters/Category/Species.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from '../Filters.module.scss';
+
+const Species = ({updatePageNumber, updateSearchSpecies}) => {
+    return(
+        <form className="d-flex justify-content-center gap-4 mb-2">
+        <input
+          onChange={(e) => {
+            updatePageNumber(1);
+            updateSearchSpecies(e.target.value);
+          }}
+          placeholder="Species"
+          type="text"
+          className={styles.input}
+        />
+            
+        </form>
+    )
+}
+
+export default Species; 
