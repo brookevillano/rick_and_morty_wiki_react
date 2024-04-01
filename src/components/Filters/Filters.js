58268cd7788a1520056d4d51745b163504ea5336
brookevillano <104024875+brookevillano@users.noreply.github.com>
2024-04-01 03:09:44 -0700

    Update Filters.js
    -  gender filter (radio select)
    -  status filter (radio select)
    - species, type and name are all input
    - need to fix styling

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React from "react";
+import Gender from "./Category/Gender";
+import Name from "./Category/Name";
+import Species from "./Category/Species";
+import Status from "./Category/Status";
+import Type from "./Category/Type";
+import styles from "./Filters.module.scss";
 
-const Filters = () => {
-    return(
-        <div>
-            Filters
-        </div>
-    )
-}
+const Filters = ({
+  updatePageNumber,
+  updateSearchName,
+  updateSearchSpecies,
+  updateSearchType,
+  updateStatus,
+  updateSearchGender,
+}) => {
+  return (
+    <div className={`d-flex align-items-start justify-content-center gap-2 ${styles.filtersContainer}`}>
+      {/* Filter by searching for a name*/}
+      <form className={`${styles.filter_component}d-flex justify-content-center gap-2 mb-5`}>
+        <Name
+          updatePageNumber={updatePageNumber}
+          updateSearchName={updateSearchName}
+        />
+      </form>
 
-export default Filters
+      {/* Filter by selecting a status*/}
+      <div className={`${styles.filter_component} d-flex justify-content-center gap-2 mb-5`}>
+        {" "}
+        <Status
+          updatePageNumber={updatePageNumber}
+          updateStatus={updateStatus}
+        />{" "}
+      </div>
+
+      {/* Filter by searching for a species*/}
+      <form className={`${styles.filter_component} d-flex justify-content-center gap-2 mb-5`}>
+        <Species
+          updatePageNumber={updatePageNumber}
+          updateSearchSpecies={updateSearchSpecies}
+        />
+      </form>
+
+      <div className={`${styles.filter_component} d-flex justify-content-center gap-2 mb-5`}>
+        {" "}
+        <Gender
+          updatePageNumber={updatePageNumber}
+          updateSearchGender={updateSearchGender}
+        />{" "}
+      </div>
+
+      {/* Filter by searching for a type*/}
+      <form className={`${styles.filter_component} d-flex justify-content-center gap-2 mb-5`}>
+        <Type
+          updatePageNumber={updatePageNumber}
+          updateSearchType={updateSearchType}
+        />
+      </form>
+
+      <div className={`${styles.filter_component} d-flex justify-content-center gap-2 mb-5`}>
+        <button
+          className={`${styles.btn} btn btn-danger fs-5 align-self-center`}
+        >
+          Clear
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
