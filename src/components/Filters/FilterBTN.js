f5550f0573b4dc2201b1288be8856c095530a58a
brookevillano <104024875+brookevillano@users.noreply.github.com>
2024-04-02 01:33:29 -0700

    Update FilterBTN.js

--- src/components/Filters/FilterBTN.js
+++ src/components/Filters/FilterBTN.js
@@ -7,7 +7,7 @@ const FilterBTN = ({ value, index, items, task, updatePageNumber }) => {
         <input
           onClick={() => {
             updatePageNumber(1);
-            task(items); // updates the api
+            task(items);        // updates the api
           }}
           class="form-check-input"
           type="radio"
