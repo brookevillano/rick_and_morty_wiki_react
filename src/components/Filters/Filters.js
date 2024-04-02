8673289a07bb69934134039744f10425004155e4
brookevillano <104024875+brookevillano@users.noreply.github.com>
2024-04-02 01:33:05 -0700

    Update Filters.js
    Made clear button functional

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Gender from "./Category/Gender";
 import Name from "./Category/Name";
 import Species from "./Category/Species";
@@ -14,53 +14,51 @@ const Filters = ({
   updateStatus,
   updateSearchGender,
 }) => {
+
+  const clear = () => {
+    updatePageNumber(1);
+    updateSearchName("");
+    updateSearchSpecies("");
+    updateSearchType("");
+    updateStatus("");
+    updateSearchGender("");
+    window.location.reload(false);
+  };
+
   return (
     <div className={`d-flex align-items-start justify-content-center gap-3`}>
-      {/* Filter by searching for a name*/}
       <div className={`${styles.filters_container} d-flex justify-content-center mb-2`}>
         <Name
           updatePageNumber={updatePageNumber}
           updateSearchName={updateSearchName}
         />
       </div>
-
-      {/* Filter by selecting a status*/}
       <div className={`${styles.filters_container} d-flex justify-content-center mb-2`}>
-        {" "}
         <Status
-          updatePageNumber={updatePageNumber}
+          updatePageNumber={updatePageNumber} 
           updateStatus={updateStatus}
-        />{" "}
+        />
       </div>
-
-      {/* Filter by searching for a species*/}
       <div className={`${styles.filters_container} d-flex justify-content-center mb-2`}>
         <Species
           updatePageNumber={updatePageNumber}
           updateSearchSpecies={updateSearchSpecies}
         />
       </div>
-
       <div className={`${styles.filters_container} d-flex justify-content-center mb-2`}>
-        {" "}
         <Gender
           updatePageNumber={updatePageNumber}
           updateSearchGender={updateSearchGender}
-        />{" "}
+        />
       </div>
-
-      {/* Filter by searching for a type*/}
       <div className={`${styles.filters_container} d-flex justify-content-center mb-2`}>
         <Type
           updatePageNumber={updatePageNumber}
           updateSearchType={updateSearchType}
         />
       </div>
-
       <div className={`d-flex justify-content-center mb-3`}>
-        <button
-          className={`${styles.btn} btn btn-danger fs-5 align-self-center`}
-        >
+        <button onClick={clear} className={`${styles.btn} btn btn-danger fs-5 align-self-center`}>
           Clear
         </button>
       </div>
